7b5bffc3efeed0dd21d667f2665aa0e883f570e5
James D. Bartlett III <jimmyb@gmail.com>
2018-10-22 10:44:23 -0500

    Updates

---
+++ precache-manifest.026f041fd9b63505d5d841ef3c85fd9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cec5678597497f7f6434",
+    "url": "/dev-moines/static/js/runtime~main.cec56785.js"
+  },
+  {
+    "revision": "f97549dcf85226c1f693",
+    "url": "/dev-moines/static/js/main.f97549dc.chunk.js"
+  },
+  {
+    "revision": "4334425ddea4ae8fec42",
+    "url": "/dev-moines/static/js/1.4334425d.chunk.js"
+  },
+  {
+    "revision": "f97549dcf85226c1f693",
+    "url": "/dev-moines/static/css/main.eea31318.chunk.css"
+  },
+  {
+    "revision": "4334425ddea4ae8fec42",
+    "url": "/dev-moines/static/css/1.ee54111e.chunk.css"
+  },
+  {
+    "revision": "7b6c8089bf9e8bc5edac3da1f886033a",
+    "url": "/dev-moines/index.html"
+  }
+];
--- src/APIs/MyJsonAPI.js
+++
@@ -1,6 +0,0 @@
-const url = 'https://api.myjson.com/bins/'
-const id = '10q460'
-
-module.exports = {
-	url, id
-}
